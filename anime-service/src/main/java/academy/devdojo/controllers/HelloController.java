07fbf59818201e0275ebf9b4241e5d9208392680
Alison8547 <alisonailson100@hotmail.com>
2025-01-25 15:14:03 -0300

    video06: RestController and GetMapping

---
+++ anime-service/src/main/java/academy/devdojo/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package academy.devdojo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping(value = {"hi", "hi/"})
+    public String hi() {
+        return "Go Ohsatsu";
+    }
+}
