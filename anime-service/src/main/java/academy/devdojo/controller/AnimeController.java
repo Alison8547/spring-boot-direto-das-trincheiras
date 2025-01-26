7d90cb55501d1520985838c3060da956ad4afdcd
Alison8547 <alisonailson100@hotmail.com>
2025-01-26 11:12:23 -0300

    video10: Virtual Threads, Stacktrace e Tomcat Threads

--- anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
+++ anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
@@ -1,17 +1,26 @@
 package academy.devdojo.controller;
 
+import lombok.extern.log4j.Log4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("v1/animes")
+@Log4j
 public class AnimeController {
 
+    private static final Logger log = LogManager.getLogger(AnimeController.class);
+
     @GetMapping
-    public List<String> getAll() {
+    public List<String> getAll() throws InterruptedException {
+        log.info(Thread.currentThread().getName());
+        TimeUnit.SECONDS.sleep(1);
         return List.of("Naruto", "Bleach", "Hunter x Hunter", "Yugioh");
     }
 }
