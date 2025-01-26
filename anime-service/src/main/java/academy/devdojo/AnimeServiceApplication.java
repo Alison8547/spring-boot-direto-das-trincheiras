62e5d09331cc55e0d240e622f292984673c3377f
Alison8547 <alisonailson100@hotmail.com>
2025-01-25 21:55:48 -0300

    video09: exercicio endpoint get list animes

--- anime-service/src/main/java/academy/devdojo/AnimeServiceApplication.java
+++ anime-service/src/main/java/academy/devdojo/AnimeServiceApplication.java
@@ -2,19 +2,13 @@ package academy.devdojo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-import java.util.Arrays;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"outside.devdojo", "academy.devdojo"}) // pra escaner o pacote outside.devdojo e instânciar seus beans
 public class AnimeServiceApplication {
 
     public static void main(String[] args) {
-        var application = SpringApplication.run(AnimeServiceApplication.class, args);
+        SpringApplication.run(AnimeServiceApplication.class, args);
 
-        Arrays.stream(application.getBeanDefinitionNames())
-                .forEach(System.out::println);
 
     }
 
