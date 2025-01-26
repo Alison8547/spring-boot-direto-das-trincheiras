e8936652cea4503123e9be2a2c69ba608b464dc5
Alison8547 <alisonailson100@hotmail.com>
2025-01-25 21:21:16 -0300

    video08: Spring Beans e ComponentScan

--- anime-service/src/main/java/academy/devdojo/AnimeServiceApplication.java
+++ anime-service/src/main/java/academy/devdojo/AnimeServiceApplication.java
@@ -2,12 +2,20 @@ package academy.devdojo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Arrays;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"outside.devdojo", "academy.devdojo"}) // pra escaner o pacote outside.devdojo e instânciar seus beans
 public class AnimeServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AnimeServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        var application = SpringApplication.run(AnimeServiceApplication.class, args);
+
+        Arrays.stream(application.getBeanDefinitionNames())
+                .forEach(System.out::println);
+
+    }
 
 }
