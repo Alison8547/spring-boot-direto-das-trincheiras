bc4f704731bbb84e1ecbcbca4c4dbaea28a8e181
Alison8547 <alisonailson100@hotmail.com>
2025-01-27 21:41:23 -0300

    video14: PostMapping

--- anime-service/src/main/java/academy/devdojo/controller/HelloController.java
+++ anime-service/src/main/java/academy/devdojo/controller/HelloController.java
@@ -1,14 +1,25 @@
 package academy.devdojo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 // @RequestMapping(value = "greeting")
 public class HelloController {
 
+    private static final Logger log = LogManager.getLogger(HelloController.class);
+
     @GetMapping(value = {"hi", "hi/"})
     public String hi() {
         return "Go Ohsatsu";
+    }
+
+    @PostMapping("save")
+    public Long save(@RequestBody String name) {
+        log.info("save {}", name);
+        return ThreadLocalRandom.current().nextLong(1, 100);
     }
 }
